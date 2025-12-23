52f5b53c86c55365af1e74d8f6b4c06bba555185
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:13:33 -0300

    Create FORnumeorsPares.java

---
+++ registros/FORnumeorsPares.java
@@ -0,0 +1,11 @@
+public class FORnumerosPares {
+    
+    public static void main(String[] args){
+        for(int i = 2; i <= 100; i += 2){
+            System.out.println("Contagem " + i);
+
+        }
+    }
+
+}
+//Commits on May 24, 2024
