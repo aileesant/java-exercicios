5e98047aff66bf99b773e92016f47b1335d72350
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:15:32 -0300

    Create Repeticao.java

---
+++ registros/Repeticao.java
@@ -0,0 +1,11 @@
+public class Repeticao {
+    public static void main (String[] args){
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+
+    }
+    
+}
+
+//Commits on May 24, 2024
