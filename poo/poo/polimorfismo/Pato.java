094f939473c72e087fc52ddcbcb3d7c048b7e872
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 17:42:35 -0300

    Create Pato.java

---
+++ poo/poo/polimorfismo/Pato.java
@@ -0,0 +1,16 @@
+package org.example.polimorfismo.ex1;
+
+public class Pato implements Animal {
+
+    @Override
+    public String emitirSom() {
+        return "Quá Quá";
+    }
+
+    @Override
+    public String comer() {
+        return "Pão";
+    }
+}
+
+//Commits on Sep 13, 2025
