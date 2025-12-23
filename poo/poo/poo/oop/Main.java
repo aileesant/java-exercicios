cb094996366d633b3d2f0fce6913f5960c1db6a0
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:46:50 -0300

    Update Main.java

--- poo/poo/poo/oop/Main.java
+++ poo/poo/poo/oop/Main.java
@@ -1,1 +1,26 @@
+package org.example.encapsulamento;
 
+public class Main {
+    public static void main(String[] args) {
+       // Instanciar a classe Cliente.
+        Cliente cliente = new Cliente();
+        Produto produto = new Produto();
+
+        // Atribuir valor.
+        // cliente.nome = "Marta";
+        // cliente.telefone = "71356691";
+        cliente.setNome("Yuta");
+        cliente.setTelefone("719123456");
+        produto.setNome("Sushi");
+        produto.setPreco(7.99);
+
+
+        // Mostrar.
+        //System.out.println("Nome: " + cliente.nome);
+        //System.out.println("Telefone: " + cliente.telefone);
+        System.out.println("Nome: " + cliente.getNome());
+        System.out.println("Telefone: " + cliente.getTelefone());
+        System.out.println("Nome: " + produto.getNome());
+        System.out.println("Preço: " + produto.getPreco());
+    }
+}
