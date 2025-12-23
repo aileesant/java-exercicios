d4c89f16df52213d9e44dea0551f24a09076bfab
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 17:40:51 -0300

    Create Galo.java

---
+++ poo/poo/polimorfismo/Galo.java
@@ -0,0 +1,16 @@
+package org.example.polimorfismo.ex1;
+
+public class Galo implements Animal {
+
+    @Override
+    public String emitirSom() {
+        return "Cocoricóoooooooo";
+    }
+
+    @Override
+    public String comer() {
+        return "Milho";
+    }
+}
+
+//Commits on Sep 13, 2025
