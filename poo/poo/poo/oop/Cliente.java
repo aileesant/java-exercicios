eff0e248858b5fcb0a92e3a64b12aa6a896954cd
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:46:28 -0300

    Create Cliente.java

---
+++ poo/poo/poo/oop/Cliente.java
@@ -0,0 +1,28 @@
+package org.example.encapsulamento;
+
+public class Cliente {
+   private String nome;
+   private String telefone;
+
+
+    //metodos de acesso
+    //set colocar um valor lá
+    //get mostrar
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
+
+//Commits on Sep 6, 2025
