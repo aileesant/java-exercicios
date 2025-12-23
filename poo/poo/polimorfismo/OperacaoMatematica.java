f027681dd963435cc223957b2d514a4c033f6400
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 17:34:40 -0300

    Create OperacaoMatematica interface for calculations

---
+++ poo/poo/polimorfismo/OperacaoMatematica.java
@@ -0,0 +1,7 @@
+package org.example.polimorfismo.ex2;
+
+public interface OperacaoMatematica {
+    // + é público no diagrama
+    // odo metodo é por padrao public abstrat, então seria reduntante
+    double calcular(double a, double b);
+}
