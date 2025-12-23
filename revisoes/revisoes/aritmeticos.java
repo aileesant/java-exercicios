4c88188510b883462ab924eefb0a7bfbf33a36a3
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:24:49 -0300

    Create aritmeticos.java

---
+++ revisoes/revisoes/aritmeticos.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Aritmeticos {
+    public static void main(String[] args) {
+        // declaracao de variaveis
+        int a = 10;
+        int b = 2;
+
+        // operadores aritmeticos
+        int multiplicar = a * b;
+        int dividir = a / b;
+        int somar = a + b;
+        int subtrair = a - b;
+
+        // exibindo resultados
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+        System.out.println("Somar: " + somar );
+        System.out.println("Subtrair: " + subtrair);
+    }
+}
