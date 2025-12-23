9c169ec92ed0c013553501eeeba9e57d2b057e43
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:24:01 -0300

    Create Lista.java

---
+++ revisoes/revisoes/Lista.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Lista {
+
+    public static void main(String[] args) {
+
+    ArrayList<Double> notas = new ArrayList<>();
+
+        notas.add(10.0);
+        notas.add(8.0);
+
+        for (double nota: notas){
+        System.out.println("Nota: " + nota);
+    }
+}
+}
+
+//Commits on Aug 30, 2025
