9eca5e66d86f935f1e44fd6197e686afcb5a7d2a
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:43:20 -0300

    Create Main.java

---
+++ revisoes/revisoes/revisoes/Main.java
@@ -0,0 +1,16 @@
+package org.example.atividade1;
+
+public class Main {
+    public static void main(String[] args) {
+      //instanciar
+        Livro livro1 = new Livro("Cartas", "Eu", 99, 89.00);
+        Livro livro2 = new Livro("Sla", "Tu", 99, 99.99);
+
+        //mostrar
+        System.out.println(livro1.toString());
+        System.out.println(livro2.toString());
+
+    }
+}
+
+//Commits on Sep 6, 2025
