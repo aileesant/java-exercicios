1937d7baa4d00ba85039321213b04978b727a1de
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:27:01 -0300

    Create laço de repetição for.java

---
+++ revisoes/revisoes/laço de repetição for.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class LacoFor {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i ++) {
+            System.out.println("Valor de i: "+ i);
+        }
+    }
+}
