a11046d7e24a46e4503dfee44d021b8444c07ada
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:25:22 -0300

    Create condicionais.java

---
+++ revisoes/revisoes/condicionais.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int[] notas = new int[2];
+
+        notas[0] = 10;
+        notas[1] = 8;
+
+        for (int i = 0; i < 2; i++){
+            System.out.println("Nota: " + notas[i]);
+        }
+    }
+}
