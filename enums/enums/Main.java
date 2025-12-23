b1fccb9f2e22b100583722eb443b0856dc818431
enilA <166937749+aileesant@users.noreply.github.com>
2025-12-23 18:02:47 -0300

    Create Main.java

---
+++ enums/enums/Main.java
@@ -0,0 +1,10 @@
+package org.example.enums;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto produto = new Produto("Mouse", pedido.CONCLUIDO);
+
+        System.out.println(produto.toString());
+}
+
+//Commits on Sep 6, 2025
